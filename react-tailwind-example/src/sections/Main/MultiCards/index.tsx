bbc852af4b089952cca3001ef9167e63815a1bb4
Nicolas Merget <nicolas.merget@deutschebahn.com>
2024-05-16 16:44:02 +0200

    fix: build in pipeline

--- react-tailwind-example/src/sections/Main/MultiCards/index.tsx
+++ react-tailwind-example/src/sections/Main/MultiCards/index.tsx
@@ -1,23 +1,23 @@
-import { DBCard, DBLink } from '@db-ui/react-components';
-import type { MultiCardsType } from './data';
+import { DBCard, DBLink } from "@db-ui/react-components";
+import type { MultiCardsType } from "./data";
 
 const MultiCards = ({ cards }: MultiCardsType) => (
-	<div className={`grid grid-cols-1 s:grid-cols-${cards.length} gap-fix-md`}>
-		<div className="hidden s:grid-cols-2 s:grid-cols-3 s:grid-cols-4" />
-		{cards.map((card) => (
-			<DBCard key={card.title} variant="interactive" imgSrc={card.image}>
-				<div className="flex flex-col gap-fix-md h-full p-fix-lg">
-					<h5>{card.title}</h5>
-					<p>{card.text}</p>
-					<div className="mt-auto">
-						<DBLink href="#" content="internal">
-							{card.link}
-						</DBLink>
-					</div>
-				</div>
-			</DBCard>
-		))}
-	</div>
+  <div className={`grid grid-cols-1 s:grid-cols-${cards.length} gap-fix-md`}>
+    <div className="hidden s:grid-cols-2 s:grid-cols-3 s:grid-cols-4" />
+    {cards.map((card) => (
+      <DBCard key={card.title}>
+        <div className="flex flex-col gap-fix-md h-full p-fix-lg">
+          <h5>{card.title}</h5>
+          <p>{card.text}</p>
+          <div className="mt-auto">
+            <DBLink href="#" content="internal">
+              {card.link}
+            </DBLink>
+          </div>
+        </div>
+      </DBCard>
+    ))}
+  </div>
 );
 
 export default MultiCards;
