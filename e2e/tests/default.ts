8a934dcae2044d9f158e9b1b4dc810122223ec05
Nicolas Merget <nicolas.merget@deutschebahn.com>
2024-08-13 09:57:16 +0200

    chore: wait another 1000 sec. for default test

--- e2e/tests/default.ts
+++ e2e/tests/default.ts
@@ -3,6 +3,7 @@ import { expect, test } from "@playwright/test";
 export const getDefaultScreenshotTest = (example: string) => {
   test(`should match screenshot`, async ({ page }) => {
     await page.goto(`./${example}`, { waitUntil: "networkidle" });
+    await page.waitForTimeout(1000);
     await expect(page).toHaveScreenshot({
       fullPage: true,
     });
